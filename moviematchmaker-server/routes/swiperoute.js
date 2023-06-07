c95efc1e66c631caf75fe4264e0dc82eddda51f3
Tonio9802984 <antoine.prevors@gmail.com>
2023-06-07 11:25:00 +0200

    modification on post

--- moviematchmaker-server/routes/swiperoute.js
+++ moviematchmaker-server/routes/swiperoute.js
@@ -5,15 +5,32 @@ const UserSwipe = require("./../models/SwipeModel");
 
 router.post("/", async (req, res, next) => {
   try {
-    const { userId, movieId, swipe } = req.body;
-    const userSwipe =
-      (await UserSwipe.findOne({ userId })) || new UserSwipe({ userId });
+    const { movieId, swipe } = req.body;
+    const { _id: userId } = req.user; // Extract the user's ID from req.user
+
+    console.log(
+      `Processing ${swipe} action for user: ${userId} on movie: ${movieId}`
+    );
+
+    let userSwipe = await UserSwipe.findOne({ userId });
+    console.log("UserSwipe found:", userSwipe);
+
+    if (!userSwipe) {
+      console.log("No UserSwipe found, creating a new one...");
+      userSwipe = new UserSwipe({ userId });
+    }
 
     userSwipe[swipe].push(movieId);
+    console.log(`Added movieId to ${swipe}: `, userSwipe[swipe]);
 
     await userSwipe.save();
+    console.log("UserSwipe saved successfully");
+
     res.status(201).send();
   } catch (error) {
+    console.error(
+      `Error processing ${swipe} action for user: ${userId} on movie: ${movieId}`
+    );
     next(error);
   }
 });
